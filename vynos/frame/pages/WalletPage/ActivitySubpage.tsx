4bcf670f0d116900013cf9d1dc5bbdd0c693006c
Matthew Slipper <me@matthewslipper.com>
2018-03-24 22:46:33 -0700

    Activity grouping

--- vynos/frame/pages/WalletPage/ActivitySubpage.tsx
+++ vynos/frame/pages/WalletPage/ActivitySubpage.tsx
@@ -21,6 +21,14 @@ export interface ActivitySubpageProps extends StateProps {
 
 export interface ActivitySubpageState {
   isLoading: boolean
+  detailRows: Set<number>
+}
+
+interface Group {
+  startDate: Date
+  endDate?: Date
+  groupKey: string
+  items: HistoryItem[]
 }
 
 class ActivitySubpage extends React.Component<ActivitySubpageProps, ActivitySubpageState> {
@@ -28,7 +36,8 @@ class ActivitySubpage extends React.Component<ActivitySubpageProps, ActivitySubp
     super(props)
 
     this.state = {
-      isLoading: false
+      isLoading: false,
+      detailRows: new Set<number>()
     }
   }
 
@@ -41,6 +50,20 @@ class ActivitySubpage extends React.Component<ActivitySubpageProps, ActivitySubp
 
     this.setState({
       isLoading: false
+    })
+  }
+
+  toggleDetails (i: number) {
+    const clone = new Set(Array.from(this.state.detailRows))
+
+    if (clone.has(i)) {
+      clone.delete(i)
+    } else {
+      clone.add(i)
+    }
+
+    this.setState({
+      detailRows: clone
     })
   }
 
@@ -58,6 +81,52 @@ class ActivitySubpage extends React.Component<ActivitySubpageProps, ActivitySubp
     const aggregateAmount = this.props.history.reduce((acc: BigNumber.BigNumber, curr: HistoryItem) => {
       return acc.plus(curr.payment.price)
     }, new BigNumber.BigNumber(0))
+
+    const groups = this.props.history.reduce((acc: Group[], curr: HistoryItem, i: number) => {
+      const date = new Date(Number(curr.createdAt))
+
+      const mon = date.getMonth()
+      const day = date.getDate()
+      const groupKey = `${mon}-${day}-${curr.streamId}`
+
+      if (!acc.length) {
+        acc.push({
+          startDate: date,
+          groupKey,
+          items: [ curr ]
+        })
+
+        return acc
+      }
+
+      const last = acc[acc.length - 1]
+
+      if (last.groupKey === groupKey) {
+        last.items.push(curr)
+
+        if (i === this.props.history.length - 1) {
+          last.endDate = date
+        }
+
+        return acc
+      }
+
+      last.endDate = new Date(last.items[last.items.length - 1].createdAt)
+
+      const group = {
+        startDate: date,
+        groupKey,
+        items: [ curr ]
+      } as any
+
+      if (i === this.props.history.length - 1) {
+        group.endDate = date
+      }
+
+      acc.push(group)
+
+      return acc
+    }, [])
 
     return (
       <div className={s.walletActivityWrapper}>
@@ -81,54 +150,94 @@ class ActivitySubpage extends React.Component<ActivitySubpageProps, ActivitySubp
               <T.TableHeaderCell
                 className={classnames(s.walletActivityHeaderCell, s.walletActivityTime)}>Time</T.TableHeaderCell>
               <T.TableHeaderCell className={s.walletActivityHeaderCell}>Item</T.TableHeaderCell>
-              <T.TableHeaderCell></T.TableHeaderCell>
-              <T.TableHeaderCell></T.TableHeaderCell>
+              <T.TableHeaderCell />
+              <T.TableHeaderCell />
             </T.TableRow>
           </T.TableHeader>
           <T.TableBody>
-            {this.renderRows()}
+            {this.renderRows(groups)}
           </T.TableBody>
         </T.Table>
       </div>
     )
   }
 
-  renderRows () {
-    return this.props.history.map((item: HistoryItem) => {
-      const date = new Date(Number(item.createdAt))
+  renderRows (groups: Group[]) {
+    return groups.map((group: Group, i: number) => {
+      const startDate = group.startDate
+      const endDate = group.endDate
 
-      const m = moment(date)
-      const mon = m.format('MMM')
-      const day = m.format('DD')
-      const time = m.format('h:mmA')
+      const mStart = moment(startDate)
+      const mEnd = moment(endDate)
 
-      return (
-        <T.TableRow key={item.id} className={s.walletActivityEntry}>
+      const toggled = this.state.detailRows.has(i)
+
+      const total = group.items.reduce((acc: BigNumber.BigNumber, curr: HistoryItem) => {
+        return acc.plus(curr.payment.price)
+      }, new BigNumber.BigNumber(0))
+
+      return [
+        <T.TableRow
+          key={`${group.groupKey}-info`}
+          className={classnames(s.walletActivityEntry, {
+            [s.walletActivityEntryToggled]: toggled
+          })}
+        >
           <T.TableCell className={s.walletActivityDate}>
-            <div className={s.walletActivityMonth}>{mon}</div>
-            <div className={s.walletActivityDay}>{day}</div>
+            <div className={s.walletActivityMonth}>{mStart.format('MMM')}</div>
+            <div className={s.walletActivityDay}>{mStart.format('DD')}</div>
           </T.TableCell>
           <T.TableCell className={s.walletActivityTime}>
-            <div className={s.walletActivityStart}>{time}</div>
+            <div className={s.walletActivityStart}>{mStart.format('h:mmA')} - <br /> {mEnd.format('h:mmA')}</div>
           </T.TableCell>
           <T.TableCell className={s.walletActivityItemDescription}>
-            <div className={s.walletActivityItem}>{item.streamName}</div>
-            <div className={s.walletActivityDescription}>{item.performerName}</div>
+            <div className={s.walletActivityItem}>{group.items[0].streamName}</div>
+            <div className={s.walletActivityDescription}>{group.items[0].performerName}</div>
           </T.TableCell>
           <T.TableCell className={s.walletActivityPrice}>
             <div className={s.walletActivitySubtract}>-</div>
             <div className={s.walletActivitySubtractAmount}>
               <Currency
-                amount={new BigNumber.BigNumber(item.payment.price)}
+                amount={total}
                 showUnit
               />
             </div>
           </T.TableCell>
           <T.TableCell className={s.walletActivityAction}>
-            <div className={s.walletActivityMore}>...</div>
+            <div className={classnames(s.walletActivityMore, {
+              [s.walletActivityMoreToggled]: toggled
+            })} onClick={() => this.toggleDetails(i)}>...</div>
           </T.TableCell>
-        </T.TableRow>
-      )
+        </T.TableRow>,
+        toggled ? group.items.map((item: HistoryItem, i: number) => (
+          <T.TableRow
+            key={`${group.groupKey}-details-${item.createdAt}`}
+            className={classnames(s.walletActivityDetails, {
+              [s.walletActivityEntryToggled]: toggled,
+              [s.walletActivityEntryFirst]: i === 0,
+              [s.walletActivityEntryLast]: i === group.items.length - 1,
+            })}
+          >
+            <T.TableCell />
+            <T.TableCell className={s.walletActivityTime}>
+              <div className={s.walletActivityStart}>{moment(new Date(item.createdAt)).format('h:mmA')}</div>
+            </T.TableCell>
+            <T.TableCell className={s.walletActivityItemDescription}>
+              <div className={s.walletActivityItem}>{item.streamName}</div>
+            </T.TableCell>
+            <T.TableCell className={s.walletActivityPrice}>
+              <div className={s.walletActivitySubtract}>-</div>
+              <div className={s.walletActivitySubtractAmount}>
+                <Currency
+                  amount={item.payment.price}
+                  showUnit
+                />
+              </div>
+            </T.TableCell>
+            <T.TableCell />
+          </T.TableRow>
+        )) : null
+      ]
     })
   }
 }
