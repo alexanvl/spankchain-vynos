b93e39b062b12ac3121e591df57f8ae56437c471
Matthew Slipper <me@matthewslipper.com>
2018-04-01 00:57:16 -0700

    Fix bug with channel ID encoding (#438)

--- harness/harness.ts
+++ harness/harness.ts
@@ -37,7 +37,7 @@ window.addEventListener("load", function () {
   const tipButton = document.getElementById('tip')
   if (tipButton) {
     tipButton.onclick = () => {
-      vynos.buy(new BigNumber.BigNumber(0), {
+      vynos.buy(new BigNumber.BigNumber(100), {
         streamId: 'abc-123',
         streamName: 'SpankCam',
         performerId: 'abc-234',
