ad9d246867a39a0a4ee3dd68b2b94e822c16a4ab
David Yoo <dyoo@uber.com>
2018-05-31 16:20:26 -0700

    Add initial selenium implementation

--- test/e2e/frame/entry.js
+++ test/e2e/frame/entry.js
@@ -1,20 +1,10 @@
 module.exports = {
-  'User can click wallet button': client => {
+  'User can signup': client => {
     const mainPage = client.page.frame.entry()
 
     mainPage
       .navigate()
       .testFrameButton()
-
-    client.end()
-  },
-
-  'User can progress with valid password': client => {
-    const mainPage = client.page.frame.entry()
-
-    mainPage
-      .navigate()
-      .openFrame()
       .testValidPassword()
 
     client.end()
