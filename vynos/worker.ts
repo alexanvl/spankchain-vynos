1cf0b420183d12e5dc4ae05467fed76536f58adc
Matthew Slipper <me@matthewslipper.com>
2018-08-31 14:22:45 -0700

    Fix migration totally

--- vynos/worker.ts
+++ vynos/worker.ts
@@ -90,21 +90,20 @@ asServiceWorker((self: ServiceWorkerGlobalScope) => {
     })
 
     const persistentKey = 'persist:persistent'
-    await localForage.setItem<any>(persistentKey, {"persistent":"{\"didInit\":true,\"rememberPath\":\"/\",\"pendingChannelIds\":[],\"keyring\":\"eyJkYXRhIjoiYWlBbkV4NS9VUElCdVVSWWg2bkd4aFl6alR0dGpBd0l3eTAwR0ZWVXMxZzRkcThkS0JiU09ZTzRVbk0zcEQxSnBaV0VXS1NHQStPTldQMFg1K0F1MlB5aXlJN3FDSEo2SWtOY1dtUUx0TnVLQ09weWlRR0hDTnJ1V0JXM2lzVy9yZVh6YVp0a1h5MTNreGFvSnVXQ1V6b3Z5LzhSUmhUU2hiNWg3Y3BqTGlHZy9zc3RPSkp1TnRuU0doVWZpOEVDTWlSQnZFQmxmaHRpZUZoSmRLWllyWTg9IiwiaXYiOiJvQjNaRjgrV2tKNkIrZFRKUXNzT0VRPT0iLCJzYWx0IjoiR1ZtZkQvUlBvZFlHNXJweUVZaCtDeHlBS2srZUUvdm15aEVRSmg5eFh4Zz0ifQ==\",\"hasActiveDeposit\":true}","_persist":"{\"version\":-1,\"rehydrated\":true}"})
     let existingStateStr = await localForage.getItem<{ persistent: string }>(persistentKey)
-
 
     let existingState: PersistentState
 
     try {
       existingState = JSON.parse(existingStateStr.persistent)
     } catch (e) {
+      console.error(e)
       existingState = INITIAL_STATE.persistent
     }
 
     // need to migrate
     if (!existingState.transactions) {
-      // await localForage.setItem<PersistentState>(persistentKey + '_backup', existingState)
+      await localForage.setItem<PersistentState>(persistentKey + '_backup', existingState)
 
       existingState = {
         didInit: existingState.didInit,
@@ -114,10 +113,10 @@ asServiceWorker((self: ServiceWorkerGlobalScope) => {
         transactions: {}
       }
 
-      await localForage.setItem<{ persist: string, _persist: string }>(persistentKey, {
-        persist: JSON.stringify(existingState),
+      await localForage.setItem<string>(persistentKey, JSON.stringify({
+        persistent: JSON.stringify(existingState),
         _persist: "{\"version\":-1,\"rehydrated\":true}"
-      })
+      }))
     }
 
     const persistConfig = {
