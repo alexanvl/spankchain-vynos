e38a70b0bf50ae8acda618d78ee2e776368dd135
Matthew Slipper <me@matthewslipper.com>
2018-08-31 12:21:44 -0700

    Remove merge artifacts

--- vynos/lib/getGasPrice.ts
+++
@@ -1,8 +0,0 @@
-import Web3 from 'web3'
-import { BigNumber } from 'bignumber.js'
-
-export function getGasPrice (web3: Web3): Promise<BigNumber> {
-  return new Promise((resolve, reject) => web3.eth.getGasPrice((err: any, data: BigNumber) => {
-    return err ? reject(err): resolve(data)
-  }))
-}
