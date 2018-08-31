d2304ff1533a2e2b5105f4a380911360ebff48b8
William Cory <willcory10@gmail.com>
2018-08-31 10:49:17 -0700

    make add trigger to Tooltip component

--- vynos/frame/components/Tooltip/index.tsx
+++ vynos/frame/components/Tooltip/index.tsx
@@ -5,16 +5,14 @@ import '!style-loader!css-loader!./unprefixedStyle.css'
 
 const s = require('./style.css')
 
-const Tooltip: React.SFC<any> = function (props) {
-  const { children, content } = props
-
+const Tooltip: React.SFC<any> = function ({children, content, trigger = 'hover'}) {
   return (
     <RCTooltip
       overlay={<React.Fragment>{content}</React.Fragment>}
       overlayClassName={s.tooltip}
       arrowContent={<div className={s.arrow}></div>}
       placement="bottom"
-      trigger={['hover']}
+      trigger={[trigger]}
       // visible={true} // makes tooltips always visible
     >
       <span className={s.trigger}>{children}</span>
