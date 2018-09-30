9c6e02b175909acfcf3176d796ee866698adaa5e
David Wolever <david@wolever.net>
2018-09-29 22:58:43 -0400

    Address review comments

--- vynos/frame/redux/selectors/cardBalance.ts
+++ vynos/frame/redux/selectors/cardBalance.ts
@@ -2,10 +2,6 @@ import {SharedState} from '../../../worker/WorkerState'
 import BN = require('bn.js')
 
 export function cardBalance (sharedState: SharedState): BN {
-<<<<<<< HEAD
-  return new BN(sharedState.channel ? sharedState.channel.balanceEth : 0)
-=======
   const channel = sharedState.channel
-  return new BN(channel.balances.ethBalance.amount)
->>>>>>> origin/develop
+  return new BN(channel ? channel.balances.ethBalance.amount : 0)
 }
