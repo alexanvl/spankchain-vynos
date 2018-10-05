b7b31b7f59f5dab4b916c8b0bd952a1b09cf7b9a
jasna todorovic <jasna@todorovic.ca>
2018-10-05 11:02:48 -0700

    update error message copy

--- vynos/frame/components/transactionStates.tsx
+++ vynos/frame/components/transactionStates.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react' // need this otherwise the file complains for having jsx
 
 export const alertMessagesLong: any = {
-  'AWAITING_ETH': <span>(copy to be updated) Please send some ETH to your SpankPay</span>, // TODO change these
-  'MIGRATING': <span>(copy to be updated) Converting to BOOTY</span>,
-  'MIGRATION_FAILED': <span>(copy to be updated) something went wrong in converting your wallet to BOOTY'</span>
+  'AWAITING_ETH': <span>In order to start tipping, send some ETH to your SpankPay account</span>,
+  'MIGRATING': <span>Getting your BOOTY ready</span>,
+  'MIGRATION_FAILED': <span>Oh no! Something went wrong. Please contact us at <a href="mailto:support@spankchain.com">support@spankchain.com</a></span>
 }
 
 export const alertMessagesShort: any = {
   'AWAITING_ETH': <span>Waiting for funds</span>, 
   'MIGRATING': <span>Funds Received <br/>Processing</span>,
-  'MIGRATION_FAILED': <span>(copy to be updated) something went wrong in converting your wallet to BOOTY'</span>,
+  'MIGRATION_FAILED': <span>Oh no! <br/>Something went wrong</span>,
   'DONE': <span>Booty Credited</span>
 }
 
